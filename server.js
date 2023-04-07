10581474697dbb0e0a22f647ace197865fcb5525
abdi-abderraouf <abdiabderraouf@gmail.com>
2023-04-07 20:56:49 +0200

    second commit

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const cors = require('cors')
 const { urlencoded } = require('express')
 const app = express()
 var corsOption = {
-    origin:"http://localhost:3001"
+    origin:"https://crudstudentabdiabderraouf.netlify.app/"
     //origin:"www.myfront.com"
     //origin:["s1","s2","s3"]
 }
